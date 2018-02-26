071953d149d126ac42da5bfac8c16c255bee62fb
vishal <vishal.maildrop@gmail.com>
2018-02-26 18:48:04 -0500

    1) Handled all anomalies in data

--- db_logic.py
+++ db_logic.py
@@ -6,29 +6,29 @@ import logging as log
 def insert_team(name, gender):
     conn = db.get_conn()
     cursor = conn.cursor()
-    sql = "INSERT INTO team(name,gender) VALUES('" + name + "','" + gender + "') ON DUPLICATE KEY UPDATE name = '" + name + "'"
+    sql = "INSERT INTO team(name,gender) VALUES(%s,%s) ON DUPLICATE KEY UPDATE name = %s"
+    cursor.execute(sql, (name, gender, name))
     log.debug(sql)
-    cursor.execute(sql)
     conn.commit()
     return
 
 
-def insert_people(name):
-    conn = db.get_conn()
-    cursor = conn.cursor()
-    sql = "INSERT INTO people(name) VALUES('" + name + "') ON DUPLICATE KEY UPDATE name = '" + name + "'"
-    log.debug(sql)
-    cursor.execute(sql)
-    conn.commit()
-    return
+# def insert_people(name):
+#     conn = db.get_conn()
+#     cursor = conn.cursor()
+#     sql = "INSERT INTO people(name) VALUES(%s) ON DUPLICATE KEY UPDATE name = %s"
+#     cursor.execute(sql, (name,name))
+#     log.debug(sql)
+#     conn.commit()
+#     return
 
 
-def insert_players(name):
+def insert_player(name, gender):
     conn = db.get_conn()
     cursor = conn.cursor()
-    sql = "INSERT INTO player(name) VALUES('" + name + "') ON DUPLICATE KEY UPDATE name = '" + name + "'"
+    sql = "INSERT INTO player(name, gender) VALUES(%s,%s) ON DUPLICATE KEY UPDATE name = %s"
+    cursor.execute(sql, (name, gender, name))
     log.debug(sql)
-    cursor.execute(sql)
     conn.commit()
     return
 
@@ -36,19 +36,19 @@ def insert_players(name):
 def insert_ground(venue, city):
     conn = db.get_conn()
     cursor = conn.cursor()
-    sql = "INSERT INTO ground(venue, city) VALUES('" + venue + "','" + city + "') ON DUPLICATE KEY UPDATE venue = '" + venue + "'"
+    sql = "INSERT INTO ground(venue, city) VALUES(%s,%s) ON DUPLICATE KEY UPDATE venue = %s"
+    cursor.execute(sql, (venue, city, venue))
     log.debug(sql)
-    cursor.execute(sql)
     conn.commit()
     return
 
 
-def get_people():
-    conn = db.get_conn()
-    sql = "SELECT id, name from people"
-    log.debug(sql)
-    df = pd.read_sql_query(sql, conn)
-    return df
+# def get_people():
+#     conn = db.get_conn()
+#     sql = "SELECT id, name from people"
+#     log.debug(sql)
+#     df = pd.read_sql_query(sql, conn)
+#     return df
 
 
 def get_teams():
@@ -76,15 +76,21 @@ def get_grounds():
 
 
 def insert_game(season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match, winner):
+    # TODO: Add match_result column
     conn = db.get_conn()
     cursor = conn.cursor()
-    if winner == '':
-        sql = "INSERT INTO game(season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(
-            season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match)
+    if winner == '' and player_match == '':
+        sql = "INSERT INTO game(season, date, competition, ground, team1, team2, toss_winner, toss_decision) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, (season, date, competition, int(ground), int(team1), int(team2), int(toss_winner), toss_decision))
+    elif winner == '':
+        sql = "INSERT INTO game(season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, (
+        season, date, competition, int(ground), int(team1), int(team2), int(toss_winner), toss_decision,
+        int(player_match)))
     else:
-        sql = "INSERT INTO game(season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match, winner) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match, winner)
+        sql = "INSERT INTO game(season, date, competition, ground, team1, team2, toss_winner, toss_decision, player_match, winner) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, (season, date, competition, int(ground), int(team1), int(team2), int(toss_winner), toss_decision, int(player_match), int(winner)))
     log.debug(sql)
-    cursor.execute(sql)
     row_id = cursor.lastrowid
     conn.commit()
     return row_id
@@ -93,9 +99,9 @@ def insert_game(season, date, competition, ground, team1, team2, toss_winner, to
 def insert_ball(ball, over, game, team, strike, nonstrike, bowler, runs, extras):
     conn = db.get_conn()
     cursor = conn.cursor()
-    sql = "INSERT INTO ball(ball, over, game, team, strike, nonstrike, bowler, runs, extras) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(ball, over, game, team, strike, nonstrike, bowler, runs, extras)
+    sql = "INSERT INTO ball(ball, over, game, team, strike, nonstrike, bowler, runs, extras) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     log.debug(sql)
-    cursor.execute(sql)
+    cursor.execute(sql, (ball, float(over), int(game), int(team), int(strike), int(nonstrike), int(bowler), int(runs), int(extras)))
     row_id = cursor.lastrowid
     conn.commit()
     return row_id
@@ -104,9 +110,9 @@ def insert_ball(ball, over, game, team, strike, nonstrike, bowler, runs, extras)
 def insert_out(ball_id, player_out, method_out):
     conn = db.get_conn()
     cursor = conn.cursor()
-    sql = "INSERT INTO wicket(ball_id, player_out, method_out) VALUES('{}','{}','{}')".format(ball_id, player_out, method_out)
+    sql = "INSERT INTO wicket(ball_id, player_out, method_out) VALUES(%s,%s,%s)"
     log.debug(sql)
-    cursor.execute(sql)
+    cursor.execute(sql, (ball_id, int(player_out), method_out))
     conn.commit()
     return
 
@@ -114,9 +120,9 @@ def insert_out(ball_id, player_out, method_out):
 def insert_lineup(player_id, game_id, position):
     conn = db.get_conn()
     cursor = conn.cursor()
-    sql = "INSERT INTO lineup(player_id, game_id, position) VALUES('{}','{}','{}')".format(player_id, game_id, position)
+    sql = "INSERT INTO lineup(player_id, game_id, position) VALUES(%s,%s,%s)"
     log.debug(sql)
-    cursor.execute(sql)
+    cursor.execute(sql, (int(player_id), int(game_id), position))
     conn.commit()
     return
 
--- main.py
+++ main.py
@@ -20,34 +20,35 @@ def process_file(file):
     venue = info.loc[info.type == 'venue'].ball.iloc[0]
     city = info.loc[info.type == 'city'].ball.iloc[0]
 
-    players = pd.unique(df.loc[df.key == 'ball'].loc[:, 'strike':'bowler'].values.ravel())
-
-    for row in teams.itertuples():
-        dblogic.insert_team(row.ball, gender)
-
-#    for row in people.itertuples():
-#        dblogic.insert_people(row.ball)
-
-    for item in players:
-        dblogic.insert_players(item)
-
-    dblogic.insert_ground(venue, city)
-
     season = info.loc[info.type == 'season'].ball.iloc[0]
     date = info.loc[info.type == 'date'].ball.iloc[0]
     competition = info.loc[info.type == 'competition'].ball.iloc[0]
     toss_winner = info.loc[info.type == 'toss_winner'].ball.iloc[0]
     toss_decision = info.loc[info.type == 'toss_decision'].ball.iloc[0]
-    player_match = info.loc[info.type == 'player_of_match'].ball.iloc[0]
-#    umpire = info.loc[info.type == 'umpire']
-#    reserve_umpire = info.loc[info.type == 'reserve_umpire'].ball.iloc[0]
-#    tv_umpire = info.loc[info.type == 'tv_umpire'].ball.iloc[0]
-#    match_referee = info.loc[info.type == 'match_referee'].ball.iloc[0]
+    #    umpire = info.loc[info.type == 'umpire']
+    #    reserve_umpire = info.loc[info.type == 'reserve_umpire'].ball.iloc[0]
+    #    tv_umpire = info.loc[info.type == 'tv_umpire'].ball.iloc[0]
+    #    match_referee = info.loc[info.type == 'match_referee'].ball.iloc[0]
+
+    players = pd.unique(df.loc[df.key == 'ball'].loc[:, 'strike':'bowler'].values.ravel())
+
+    process_teams(teams, gender)
+    process_players(players, gender)
+    process_ground(venue, city)
+
+#    for row in people.itertuples():
+#        dblogic.insert_people(row.ball)
 
     ground_ids = dblogic.get_grounds()
     team_ids = dblogic.get_teams()
     player_ids = dblogic.get_players()
 #    people_ids = dblogic.get_people()
+
+    if not info.loc[info.type == 'player_of_match'].empty:
+        player_match = info.loc[info.type == 'player_of_match'].ball.iloc[0]
+        player_match_id = player_ids.loc[player_ids.name == player_match].id.iloc[0]
+    else:
+        player_match_id = ""
 
     if not info.loc[info.type == 'winner'].empty:
         winner = info.loc[info.type == 'winner'].ball.iloc[0]
@@ -61,10 +62,10 @@ def process_file(file):
         competition,
         ground_ids.loc[ground_ids.venue == venue].id.iloc[0],
         team_ids.loc[team_ids.name == teams.ix[1].ball].id.iloc[0],
-        team_ids.loc[team_ids.name == teams.ix[1].ball].id.iloc[0],
+        team_ids.loc[team_ids.name == teams.ix[1].ball].id.iloc[0], # TODO: Use iloc
         team_ids.loc[team_ids.name == toss_winner].id.iloc[0],
         toss_decision,
-        player_ids.loc[player_ids.name == player_match].id.iloc[0],
+        player_match_id,
 #        people_ids.loc[people_ids.name == umpire.iloc[0].ball].id.iloc[0],
 #        people_ids.loc[people_ids.name == umpire.iloc[1].ball].id.iloc[0],
 #        people_ids.loc[people_ids.name == reserve_umpire].id.iloc[0],
@@ -73,8 +74,60 @@ def process_file(file):
         winner_id
     )
 
-    ball_no = 0
+    process_balls(balls, game_id, player_ids, teams, team_ids)
 
+    # ball_no = 0
+    # for row in balls.itertuples():
+    #     ball_no += 1
+    #     ball_id = dblogic.insert_ball(
+    #         ball_no,
+    #         row.ball,
+    #         game_id,
+    #         team_ids.loc[team_ids.name == row.strike_team].id.iloc[0],
+    #         player_ids.loc[player_ids.name == row.strike].id.iloc[0],
+    #         player_ids.loc[player_ids.name == row.nonstrike].id.iloc[0],
+    #         player_ids.loc[player_ids.name == row.bowler].id.iloc[0],
+    #         int(row.runs),
+    #         int(row.extras)
+    #     )
+    #
+    #     if not pd.isna(row.out_type):
+    #         dblogic.insert_out(
+    #             ball_id,
+    #             player_ids.loc[player_ids.name == row.out_player].id.iloc[0],
+    #             row.out_type
+    #         )
+    #
+    # # enter position data
+    # balls2 = balls.drop_duplicates(['strike'], keep='first')
+    #
+    # for team in teams.itertuples():
+    #     position_count = 0
+    #     for row in balls2.loc[balls2.strike_team == team.ball].itertuples():
+    #         position_count += 1
+    #         dblogic.insert_lineup(
+    #             player_ids.loc[player_ids.name == row.strike].id.iloc[0],
+    #             game_id,
+    #             position_count
+    #         )
+
+
+def process_teams(teams, gender):
+    for row in teams.itertuples():
+        dblogic.insert_team(row.ball, gender)
+
+
+def process_players(players, gender):
+    for item in players:
+        dblogic.insert_player(item, gender)
+
+
+def process_ground(venue, city):
+    city = "abc"
+    dblogic.insert_ground(venue, city)
+
+def process_balls(balls, game_id, player_ids, teams, team_ids):
+    ball_no = 0
     for row in balls.itertuples():
         ball_no += 1
         ball_id = dblogic.insert_ball(
@@ -109,12 +162,11 @@ def process_file(file):
                 position_count
             )
 
-
 def main():
     log.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=log.INFO)
 
     directory = "C:\\Users\\visha\\Desktop\\ipl_csv"
-#    directory = "."
+    #directory = "."
     for filename in os.listdir(directory):
         if filename.endswith(".csv"):
             log.info("Processing file: {} ...".format(filename))
@@ -122,20 +174,13 @@ def main():
             process_file(directory + "\\" + filename)
             log.info("Processed file: {} in {} secs".format(filename,time.time()-start_time))
 
-
     db.get_conn().close()
-
 
 
 if __name__ == '__main__':
     main()
 
 
-#print("Season:" + season)
-#print("Date:" + date)
-#print("Competition:" + competition)
-#print(ground_ids.loc[ground_ids.venue == venue].id.iloc[0])
-#print(umpire)
 #print("Umpire1:" + umpire.iloc[0].ball)
 #print(people_ids.loc[people_ids.name == umpire.iloc[1].ball].id.iloc[0])
 #print(player_ids.loc[player_ids.name == player_match].id.iloc[0])
